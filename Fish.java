0fe625dd3292a2eb9ce3ad4bf8efc91eb2e97fdd
balu112YT <bazsayt@gmail.com>
2026-05-12 18:13:28 +0200

    Updated
    2026 / May / 12

--- Fish.java
+++ Fish.java
@@ -1,10 +1,10 @@
 public class Fish {
-    int id; //To find fish easier down the line
+    int id;
     String name;
     String rarity;
     double minWeight, maxWeight;
     double pricePerKg;
-    int zone; //The zone the fish found in
+    int zone;
 
     public Fish(int id, String name, String rarity, double minWeight, double maxWeight, double pricePerKg, int zone) {
         this.id = id;
@@ -14,5 +14,21 @@ public class Fish {
         this.maxWeight = maxWeight;
         this.pricePerKg = pricePerKg;
         this.zone = zone;
+    }
+
+    // Calculates the price for each fish.
+    public int calculateValue(double weight) {
+        return (int) (weight * pricePerKg);
+    }
+
+    public String getColor() {
+        return switch (rarity) {
+            case "Common" -> "\u001B[37m"; // White
+            case "Rare" -> "\u001B[32m";   // Green
+            case "Epic" -> "\u001B[35m";   // Purple
+            case "Mythic" -> "\u001B[31m"; // Red
+            case "Legendary" -> "\u001B[33m"; // Yellow
+            default -> "\u001B[0m";        // Reset
+        };
     }
 }
