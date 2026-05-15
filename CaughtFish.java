92ff14b4e332ad79b110116ed8f45807cf767c8d
23ctothm-alt <23c_tothm@nyirszikszi.hu>
2026-05-15 18:04:16 +0200

    Added CaughtFish.java
    Additonal plugin

---
+++ CaughtFish.java
@@ -0,0 +1,9 @@
+public class CaughtFish {
+    public int fishId;
+    public double weight;
+
+    public CaughtFish(int fishId, double weight) {
+        this.fishId = fishId;
+        this.weight = weight;
+    }
+}
